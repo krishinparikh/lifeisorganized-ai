0dffe07aa73cc595e83ac7e6f3854d94493086c2
Krishin Parikh <krishinparikh@gmail.com>
2026-01-02 00:55:27 -0600

    clickable logo

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     setTimeout(() => {
       setShowChat(true)
       setOpacity(1)
-    }, 200)
+    }, 300)
   }
 
   useEffect(() => {
@@ -23,7 +23,14 @@ export default function Home() {
   return (
     <div style={{ opacity }} className={`flex flex-col items-center h-screen overflow-hidden px-72 pb-10 transition-opacity duration-1000 ${!showChat ? 'bg-gradient-to-b from-[#5cc7cd]/20 to-[#be82b9]/20 justify-center' : 'pt-12'}`}>
       <div className="absolute top-4 left-4">
-        <Image src="/Logo.png" alt="Logo" width={120} height={100} />
+        <Image
+          src="/Logo.png"
+          alt="Logo"
+          width={120}
+          height={100}
+          className="cursor-pointer"
+          onClick={() => window.location.reload()}
+        />
       </div>
       {!showChat && (
         <div className="w-full max-w-3xl">
