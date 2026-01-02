0046976fe29a45fdaf4da0ac9c9bd596d45bf898
Krishin Parikh <krishinparikh@gmail.com>
2026-01-02 00:52:17 -0600

    stopped error

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
   }, [])
 
   return (
-    <div style={{ opacity }} className={`flex flex-col items-center h-screen overflow-hidden px-72 pb-10 transition-opacity duration-500 ${!showChat ? 'bg-gradient-to-b from-[#5cc7cd]/20 to-[#be82b9]/20 justify-center' : 'pt-12'}`}>
+    <div style={{ opacity }} className={`flex flex-col items-center h-screen overflow-hidden px-72 pb-10 transition-opacity duration-1000 ${!showChat ? 'bg-gradient-to-b from-[#5cc7cd]/20 to-[#be82b9]/20 justify-center' : 'pt-12'}`}>
       <div className="absolute top-4 left-4">
         <Image src="/Logo.png" alt="Logo" width={120} height={100} />
       </div>
